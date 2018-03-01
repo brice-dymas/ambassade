b6b996df019a6a92dfc3544b1146cc9d276ba731
Gervais Naoussi <gervaisnaoussi@gmail.com>
2018-02-28 21:19:22 -0500

    ajout des formulaires de recherche pour les montants

---
+++ src/main/webapp/app/entities/montant/montant-search.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'jhi-montant-search',
+  templateUrl: './montant-search.component.html',
+  styles: []
+})
+export class MontantSearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
