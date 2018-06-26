31dfbf40dc10850a2eac26b7fa6c2e3b281f8eaf
Brice Dymas <briceguemkam@gmail.com>
2018-06-26 11:45:12 +0100

    Integration de ng-confirmations dans visa et passeport

--- src/main/webapp/app/entities/visa/visa-detail.component.ts
+++ src/main/webapp/app/entities/visa/visa-detail.component.ts
@@ -6,6 +6,7 @@ import { JhiEventManager, JhiDataUtils } from 'ng-jhipster';
 
 import { Visa } from './visa.model';
 import { VisaService } from './visa.service';
+import {ConfirmationService, ResolveEmit} from '@jaspero/ng-confirmations';
 
 @Component({
     selector: 'jhi-visa-detail',
@@ -21,7 +22,8 @@ export class VisaDetailComponent implements OnInit, OnDestroy {
         private eventManager: JhiEventManager,
         private dataUtils: JhiDataUtils,
         private visaService: VisaService,
-        private route: ActivatedRoute
+        private route: ActivatedRoute,
+        private _confirmation: ConfirmationService
     ) {
     }
 
@@ -66,6 +68,42 @@ export class VisaDetailComponent implements OnInit, OnDestroy {
                 this.visa = visaResponse.body;
             });
     }
+
+    payerVisa(resolved: boolean) {
+        if (resolved === true) {
+        this.visaService.payer(this.visa.id)
+            .subscribe((visaResponse: HttpResponse<Visa>) => {
+                this.visa = visaResponse.body;
+            });
+    }
+    }
+
+    encoursVisa(resolved: boolean) {
+        if (resolved === true) {
+        this.visaService.enCours(this.visa.id)
+            .subscribe((visaResponse: HttpResponse<Visa>) => {
+                this.visa = visaResponse.body;
+            });
+    }
+    }
+
+    retirerVisa(resolved: boolean) {
+        if (resolved === true) {
+        this.visaService.retirer(this.visa.id)
+            .subscribe((visaResponse: HttpResponse<Visa>) => {
+                this.visa = visaResponse.body;
+            });
+    }
+    }
+
+    pretVisa(resolved: boolean) {
+        if (resolved === true) {
+        this.visaService.pret(this.visa.id)
+            .subscribe((visaResponse: HttpResponse<Visa>) => {
+                this.visa = visaResponse.body;
+            });
+    }
+    }
     byteSize(field) {
         return this.dataUtils.byteSize(field);
     }
@@ -87,5 +125,40 @@ export class VisaDetailComponent implements OnInit, OnDestroy {
             'visaListModification',
             (response) => this.load(this.visa.id)
         );
+    }
+
+    create() {
+        this.open(this.visa);
+        // console.log('entree dans la methode create de BUTTON');
+        // const val = this.form.getRawValue();
+        // const options = Object.assign({}, val);
+        //
+        // delete options.message;
+        // delete options.title;
+        // const options: ConfirmSettings = null;
+        // options.confirmText = 'Oui je veux';
+        // options.declineText = 'Non je veux pas';
+        // options.showCloseButton(true);
+
+        // this.open('Verification de securite', 'Voulez-vous vraiment Effectuer cette tache ?');
+    }
+
+    open(visa: Visa) {
+        if (visa.state.toLocaleString() === 'NOUVEAU') {
+            this._confirmation.create('Vérification de sécurité',
+                'Voulez-vous vraiment marquer ce visa comme étant Payé ?').subscribe((ans: ResolveEmit) => this.payerVisa(ans.resolved));
+        }
+        if (visa.state.toLocaleString() === 'PAYE') {
+            this._confirmation.create('Vérification de sécurité',
+                'Voulez-vous vraiment marquer ce visa comme étant en cours de traitement ? ').subscribe((ans: ResolveEmit) => this.encoursVisa(ans.resolved));
+        }
+        if (visa.state.toLocaleString() === 'ENCOURS') {
+            this._confirmation.create('Vérification de sécurité',
+                'Voulez-vous vraiment marquer ce visa comme étant prêt a être retiré ?').subscribe((ans: ResolveEmit) => this.pretVisa(ans.resolved));
+        }
+        if (visa.state.toLocaleString() === 'PRET') {
+            this._confirmation.create('Vérification de sécurité',
+                'Voulez-vous vraiment marquer ce visa comme étant retiré ?').subscribe((ans: ResolveEmit) => this.retirerVisa(ans.resolved));
+        }
     }
 }
